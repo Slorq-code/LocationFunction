d569909c097f9e71141aca53ba58dcc4e478a570
andres <andresfra65@gmail.com>
2023-07-30 02:50:18 -0500

    despedida

--- utilidades.js
+++ utilidades.js
@@ -3,12 +3,12 @@ alert("Esta interfaz no se ve bien en un movil")
 function mostrarMensaje() {
   setTimeout(function() {
     alert("Esta interfaz está diseñada para ordenadores.");
-  }, 9000);
+  }, 15000);
 }
 function mostrarSaludo() {
     setTimeout(function() {
         alert("Siempre disponible Andres F. Rodriguez. Desarrollador mobile & front end");
-    }, 30000);
+    }, 60000);
 }
 mostrarMensaje();
 mostrarSaludo();
