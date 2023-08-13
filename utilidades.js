2af614aacd1973890087c822e3e3108f3c962af4
andres <andresfra65@gmail.com>
2023-08-12 23:55:34 -0500

    update: add all functionality

--- utilidades.js
+++ utilidades.js
@@ -1,14 +1,13 @@
-// alert("Esta interfaz no se ve bien en un movil")
-
-// function mostrarMensaje() {
-//   setTimeout(function() {
-//     alert("Esta interfaz está diseñada para ordenadores.");
-//   }, 15000);
-// }
-// function mostrarSaludo() {
-//     setTimeout(function() {
-//         alert("Siempre disponible Andres F. Rodriguez. Desarrollador mobile & front end");
-//     }, 60000);
-// }
-// mostrarMensaje();
-// mostrarSaludo();
+//  alert("Esta interfaz no se ve bien en un movil")
+//  function mostrarMensaje() {
+//    setTimeout(function() {
+//      alert("Esta interfaz está diseñada para ordenadores.");
+//    }, 15000);
+//  }
+//  function mostrarSaludo() {
+//      setTimeout(function() {
+//          alert("Siempre disponible Andres F. Rodriguez. Desarrollador mobile & front end");
+//      }, 60000);
+//  }
+//  mostrarMensaje();
+//  mostrarSaludo();
